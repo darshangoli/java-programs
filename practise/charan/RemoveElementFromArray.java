429e605cb0158211c9ee19fcf93b0f2c5bd0ea29
darshangoli <satyadarshan4022@gmail.com>
2025-11-13 21:41:53 +0530

    added remove element from array program

---
+++ practise/charan/RemoveElementFromArray.java
@@ -0,0 +1,19 @@
+package charan;
+
+public class RemoveElementFromArray {
+    static void remove(int[] arr, int num){
+        int[] newArray = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            if(num != arr[i]){
+                newArray[i] = arr[i];
+            }
+        }
+        for(int i : newArray){
+            System.out.print(i+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,3,1,2};
+        remove(arr, 4);
+    }
+}
