5177403bbdbd66a65cf94c1cd3aff8cf530d4447
darshangoli <satyadarshan4022@gmail.com>
2025-12-26 00:02:56 +0530

    convertzerosto10s

--- practise/strivers/Arrays/ConvertZerosToTens.java
+++ practise/strivers/Arrays/ConvertZerosToTens.java
@@ -3,12 +3,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ConvertZerosToTens {
-    public List<Integer> convert(int[] arr){
+    public static List<Integer> convert(int[] arr){
         List<Integer> list = new ArrayList<>();
-        for(int i : arr){
+        for(int i=0; i<arr.length; i++){
             list.add(arr[i]);
         }
         return list.stream().map(n -> n == 0 ? 10 : n).collect(Collectors.toList());
-
-    }   
+    }  
+    public static void main(String[] args) {
+        int[] arr = {3,0,40,2,0,12};
+        System.out.println(convert(arr));
+    }
+} 
 
