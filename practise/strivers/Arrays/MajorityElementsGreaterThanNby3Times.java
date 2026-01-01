d0ba7e869a350e7ea72d7b1956a01df7f3f3ce7b
darshangoli <satyadarshan4022@gmail.com>
2026-01-01 22:48:50 +0530

    majority element greater than n by 3

---
+++ practise/strivers/Arrays/MajorityElementsGreaterThanNby3Times.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MajorityElementsGreaterThanNby3Times {
+    private static List<Integer> findMajority(int[] arr){
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        int threshHold = arr.length/3;
+        for(int num : arr){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            }
+            else{
+                map.put(num, 1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() > threshHold){
+                list.add(entry.getKey());
+            }
+        }
+        return list;
+    } 
+    public static void main(String[] args) {
+        int[] arr = {2,1,2,2,3,2,5,5,5,5};
+        System.out.println(findMajority(arr));
+    }
+}
--- practise/strivers/Map/NonRepeatingCharInString.java
+++ practise/strivers/Map/NonRepeatingCharInString.java
@@ -1,23 +1,26 @@
 package strivers.Map;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NonRepeatingCharInString {
-    char find(String str){
+    List<Character> find(String str){
+        List<Character> chars = new ArrayList<>();
         Map<Character, Integer> map = new LinkedHashMap<>();
         for(char ch : str.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
         for(Map.Entry<Character, Integer> entry : map.entrySet()){
             if(entry.getValue().equals(1)){
-                return (char)entry.getKey();
+                chars.add(entry.getKey());
             }
         }
-        return '-';
+        return chars;
     }
     public static void main(String[] args) {
-        String str = "ddarrsshnhan";
+        String str = "darshan";
         NonRepeatingCharInString obj = new NonRepeatingCharInString();
         System.out.println(obj.find(str));
     }
