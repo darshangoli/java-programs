b8803c16a87401671ff06c9a3dc3ad1bad260e5e
darshangoli <satyadarshan4022@gmail.com>
2026-01-03 00:13:53 +0530

    modified twosum target

--- practise/strivers/Arrays/SumOfTwoEqualTarget.java
+++ practise/strivers/Arrays/SumOfTwoEqualTarget.java
@@ -1,18 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class SumOfTwoEqualTarget {
-    public void complement(int[] arr, int target){
-        for(int i : arr){
-            for(int j : arr){
-                if(arr[i] + arr[j] == target){
-                    System.out.println(arr[i]+" "+arr[j]);
-                }
+    public Map<Integer,Integer> complement(int[] arr, int target){
+        Map<Integer,Boolean> seen = new HashMap<>();
+        Map<Integer,Integer> result = new HashMap<>();
+        
+        for(int num : arr){
+            int complement = target - num;
+
+            if(seen.containsKey(complement)){
+                result.put(complement, num);
             }
+            seen.put(num, true);
         }
+        
+        return result;
     }
     public static void main(String[] args) {
         SumOfTwoEqualTarget obj = new SumOfTwoEqualTarget();
         int[] arr = {1,2,3,4,5,6,7,8,9,0};
         int target = 9;
-        obj.complement(arr, target);
+        System.out.println(obj.complement(arr, target));
     }
 }
