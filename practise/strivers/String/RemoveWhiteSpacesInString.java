f6694933cac344a03a0ba17531300500deef2b65
darshangoli <satyadarshan4022@gmail.com>
2026-01-05 23:09:44 +0530

    remove whiote spaces

---
+++ practise/strivers/String/RemoveWhiteSpacesInString.java
@@ -0,0 +1,20 @@
+package strivers.String;
+
+public class RemoveWhiteSpacesInString {
+    public String removeSpaces(String str) {
+        char[] charArray = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char c : charArray) {
+            if (c != ' ') {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "Darshan is Good";
+        RemoveWhiteSpacesInString obj = new RemoveWhiteSpacesInString();
+        System.out.println(obj.removeSpaces(str));
+    }
+}
