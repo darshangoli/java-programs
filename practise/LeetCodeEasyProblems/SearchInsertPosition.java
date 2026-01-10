8faa7c32f3f4a44a285b5741c54b1db147684cfe
darshangoli <satyadarshan4022@gmail.com>
2026-01-10 23:12:51 +0530

    refactor: improve searchOrInsert method and enhance logging

--- practise/LeetCodeEasyProblems/SearchInsertPosition.java
+++ practise/LeetCodeEasyProblems/SearchInsertPosition.java
@@ -1,26 +1,32 @@
 package LeetCodeEasyProblems;
 
-public class SearchInsertPosition{
-    public int searchOrInsert(int[] nums, int target){
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class SearchInsertPosition {
+    private static final Logger log = Logger.getLogger(SearchInsertPosition.class.getName());
+
+    public int searchOrInsert(int[] nums, int target) {
+        Arrays.sort(nums);
+        log.info(Arrays.toString(nums));
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = (left + right)/2;
-            if(target == mid){
+            int mid = (left + right) / 2;
+            if (target == nums[mid]) {
                 return mid;
-            }
-            else if(target < nums[mid]){
+            } else if (target < nums[mid]) {
                 right = mid - 1;
-            }
-            else{
+            } else {
                 left = mid + 1;
             }
         }
         return left;
     }
+
     public static void main(String[] args) {
         SearchInsertPosition obj = new SearchInsertPosition();
-        int[] num = {1,3,5,6};
-        System.out.println(obj.searchOrInsert(num, 4));
+        int[] num = { 1, 3, 5, 6, 9, 8, 2 };
+        System.out.println("result: "+obj.searchOrInsert(num, 4));
     }
 }
