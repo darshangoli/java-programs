72a68e6e75cb25a95be72b1a35e0a4c4f324ca67
darshangoli <satyadarshan4022@gmail.com>
2026-02-04 00:12:29 +0530

    feat: refactor canConstruct method to use character frequency map for string containment check

--- Daily_Code/LeetCodeEasyProblems/StringAContainsInStringB.java
+++ Daily_Code/LeetCodeEasyProblems/StringAContainsInStringB.java
@@ -1,14 +1,24 @@
 package Daily_Code.LeetCodeEasyProblems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringAContainsInStringB {
-    boolean canConstruct(String a, String b){
-        if(a.contains(b)){
-            return true;
+    boolean canConstruct(String str1, String str2){
+        Map<Character,Integer> map = new HashMap<>();
+        for(char ch : str2.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        return false;
+        for(char ch : str1.toCharArray()){
+            if(!map.containsKey(ch) || map.get(ch) == 0){
+                return false;
+            }
+            map.put(ch, map.get(ch)-1);
+        }
+        return true;
     }
     public static void main(String[] args) {
         StringAContainsInStringB obj = new StringAContainsInStringB();
-        System.out.println(obj.canConstruct("darshan", "han"));
+        System.out.println(obj.canConstruct("han", "darshan"));
     }
 }
